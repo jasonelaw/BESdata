ed8563b54d7d04130d7cfb3ec3e10408aeff36e2
Jason Law <jasonelaw@users.noreply.github.com>
2022-09-16 11:30:46 -0700

    Update read.esri docs

--- R/read.esri.R
+++ R/read.esri.R
@@ -1,3 +1,12 @@
+#' Read from spatial data from City of Portland spatial databases
+#'
+#' Reads from a defined list of databases.
+#'
+#'@param database Either EGH_PUBLIC on GISDB1 or SWSP on BESDBPROD1
+#'@param layer a fully qualified SQL table name, `[schema].[table]`
+#'@param query a query to submit directly to server; `Shape.STAsBinary() as geometry` is necessary in select statement
+#'@param ... additional arguments passed to st_read. The correct CRS, usually 2913, will need to be passed.
+#'@returns an `sf` object, additionally inheriting `tbl_df`, `tbl`
 #'@export
 read.esri <- function(database = "EGH_PUBLIC", layer, query = NULL, ...){
   on.exit(dbDisconnect(con))
@@ -9,7 +18,7 @@ read.esri <- function(database = "EGH_PUBLIC", layer, query = NULL, ...){
          WHERE SHAPE IS NOT NULL;"
     )
   }
-  #dbGetQuery(con, statement = glue::glue(query))
+
   sf::st_read(
     dsn             = con,
     geometry_column = "geometry",
@@ -18,34 +27,3 @@ read.esri <- function(database = "EGH_PUBLIC", layer, query = NULL, ...){
     ...
   )
 }
-
-# ESRI <- function(server, database, schema) {
-#   drivers <- unique(odbcListDrivers()$name)
-#   if ("ODBC Driver 17 for SQL Server" %in% drivers){
-#
-#   }
-#   string <- "driver={{ODBC Driver 17 for SQL Server}};server={server};database={database};schema={schema};trusted_connection=YES"
-#   glue::glue(string)
-#   # db_conn <- DBI::dbConnect(
-#   #   drv = odbc::odbc(),
-#   #   driver = "SQL Server",
-#   #   server = "GISDB1",
-#   #   database = "EGH_PUBLIC",
-#   #   schema = "ArcMap_Admin",
-#   #   trusted_connection = "True"
-#   # )
-# }
-
-# library(odbc)
-# library(sf)
-# con <- odbc::dbConnect(odbc::odbc(), .connection_string = ESRI())
-# template <- "SELECT {toString(fields)}, Shape.STAsBinary() as [geometry] FROM {layer} WHERE 1 = 1;"
-# layer <- "[ArcMap_Admin].[CENSUS_TRACTS_2010_PDX]"
-# fields <- setdiff(dbListFields(con, "CENSUS_TRACTS_2010_PDX"), "Shape")
-# qry <- glue::glue(template)
-# tmp <- dbGetQuery(
-#   conn = con,
-#   statement = qry
-# )
-# tmp$geometry <- st_as_sfc(tmp$geometry)
-# tmp2 <- st_as_sf(tmp, wkt = "geometry", crs = 2913)
